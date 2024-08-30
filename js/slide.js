c7d22c3cb2ccaff9d303ff81c2c91d874f038a67
jeon eun ha <dmsgk3636@naver.com>
2024-08-30 18:19:12 +0900

    20240830

--- js/slide.js
+++ js/slide.js
@@ -7,5 +7,31 @@ $(function () {
     centerMode: true,
     prevArrow: $(".left_grey_1"), //이전 화살표만 변경
     nextArrow: $(".right_grey_1"), //다음 화살표만 변경
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
