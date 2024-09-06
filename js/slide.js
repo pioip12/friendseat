8999d672cb4830645e5be26db06023f661ad31a0
jeon eun ha <dmsgk3636@naver.com>
2024-09-06 10:48:50 +0900

    20240906

--- js/slide.js
+++ js/slide.js
@@ -17,7 +17,7 @@ $(function () {
     nextArrow: $("#slick-slide-control01"), //다음 화살표만 변경
   });
 
-  $(".sec02_box").slick({
+  $(".sec02_all_box").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -29,8 +29,62 @@ $(function () {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $("#slick-slide-control00").on("click", function () {
+    $(this).css({
+      backgroundColor: "black",
+    });
+    $("#slick-slide-control01 ").css({
+      backgroundColor: "#fff",
+    });
+  });
+
+  $("#slick-slide-control01").on("click", function () {
+    $(this).css({
+      backgroundColor: "black",
+    });
+    $("#slick-slide-control00 ").css({
+      backgroundColor: "#fff",
+    });
+  });
+
+  $(".sec02_all_box").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 1,
+    centerMode: false,
+    prevArrow: $(".left_grey_1"), //이전 화살표만 변경
+    nextArrow: $(".right_grey_1"), //다음 화살표만 변경
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
